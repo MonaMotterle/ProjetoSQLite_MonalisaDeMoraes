b75f1f57500e03f489b98c1bc862f0541b0e92eb
Mona Motterle <monalisamoraeszm@gmail.com>
2017-12-01 00:45:51 -0200

    reorganização do cod

--- database.py
+++ database.py
@@ -26,30 +26,41 @@ class BancoDeDados:
 			CREATE TABLE IF NOT EXISTS clientes (
 					id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 					nome TEXT NOT NULL,
+					senha VARCHAR(20) NOT NULL,
 					cpf VARCHAR(11) UNIQUE NOT NULL,
 					email TEXT NOT NULL
 			);
 			""")
 
 		except AttributeError:
-			print('Faça a conexão do banco antes de criar as tabelas.')
+			print('Faça a conexão do banco.')
 
-	def inserir_cliente(self, nome, cpf, email):
+	def inserir_cliente(self, nome, senha, cpf, email):
 		"""Insere cliente no banco"""
 		try:
 			cursor = self.conexao.cursor()
 
 			try:
 				cursor.execute("""
-					INSERT INTO clientes (nome, cpf, email) VALUES (?,?,?)
-				""", (nome, cpf, email))
+					INSERT INTO clientes (nome, senha, cpf, email) VALUES (?,?,?,?)
+				""", (nome, senha, cpf, email))
 			except sqlite3.IntegrityError:
 				print('O cpf %s já existe!' % cpf)
 
 			self.conexao.commit()
 
 		except AttributeError:
-			print('Faça a conexão do banco antes de inserir clientes.')
+			print('Faça a conexão do banco.')
+	def login(self, username, senha):
+		try:
+			cursor = self.conexao.cursor()
+			sql = 'SELECT * FROM clientes WHERE nome=? and senha=?'
+			cliente = cursor.execute(sql, [username, senha]).fetchone()
+			if cliente:
+				return True
+			return False
+		except AttributeError:
+			print('Faça a conexão do banco.')
 
 	def buscar_cliente(self, cpf):
 		"""Busca um cliente pelo cpf"""
@@ -57,38 +68,33 @@ class BancoDeDados:
 			cursor = self.conexao.cursor()
 
 			# obtém todos os dados
-			cursor.execute("""SELECT * FROM clientes;""")
+			cursor.execute("SELECT * FROM clientes WHERE cpf=?", [(cpf)])
 
-			x=0
-			for linha in cursor.fetchall():
-				if linha[2] == cpf:
-					print('Cliente %s encontrado.' % linha[1])
-					x+=1
-					break
-			if x==0:
-				print ('Cliente não encontrado')
-
+			cliente = cursor.fetchone()
+			
+			if cliente:
+				print ('Cliente %s encontrado.' % cliente[1])
+			else:
+				print('Cliente não encontrado')
 		except AttributeError:
-			print('Faça a conexão do banco antes de buscar clientes.')
+			print('Faça a conexão do banco.')
 
 	def buscar_email(self, email):
 		"""Busca um cliente pelo email"""
 		try:
 			cursor = self.conexao.cursor()
 		
-			cursor.execute("""SELECT * FROM clientes;""")
+			cursor.execute("SELECT * FROM clientes WHERE email=?", [(email)])
 
-			x=0
-			for linha in cursor.fetchall():
-				if linha[3] == email:
-					print('Cliente %s encontrado.' % linha[1])
-					x+=1
-					break
-			if x==0:
-				print ('Cliente não encontrado')
+			cliente = cursor.fetchone()
+			
+			if cliente:
+				print ('Cliente %s encontrado.' % cliente[1])
+			else:
+				print('Cliente não encontrado')
 
 		except AttributeError:
-			print('Faça a conexão do banco antes de buscar clientes.')
+			print('Faça a conexão do banco.')
 
 	def remover_cliente(self, cpf):
 		"""Deleta um cliente pelo cpf"""
@@ -99,8 +105,7 @@ class BancoDeDados:
 			cursor.execute(sql, (cpf,))
 
 			self.conexao.commit()
+			print ('Cliente removido.')
 				
 		except AttributeError:
-			print('Faça a conexão do banco antes de deletar clientes.')
-
-
+			print('Faça a conexão do banco.')
--- main.py
+++ main.py
@@ -5,12 +5,10 @@ if __name__ == "__main__":
 	banco = BancoDeDados()
 	banco.conecta()
 	banco.criar_tabelas()
-	#banco.inserir_cliente('Marcos', '11111111111', 'mcastrosouza@live.com')
-	#banco.inserir_cliente('Thomas', '22222222222', 'thomas@gmail.com')
-	#banco.buscar_cliente('11111111111')
+	#banco.inserir_cliente('marcos', 'python', '11111111111', 'mcastrosouza@live.com')
+	#banco.inserir_cliente('thomas', 'javascript', '22222222222', 'thomas@gmail.com')
+	#print(banco.login('thomas', 'javascript'))
 	#banco.buscar_email('thomas@gmail.com')
-	#banco.remover_cliente('11111111111')
-
-
-
+	#banco.buscar_cliente('11111111111')
+	#banco.remover_cliente('22222222222')
 	banco.desconecta()
